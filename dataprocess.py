ecd2292687afdf7734cf74901e7cd65dccfdcde2
nantsing <nanqing_zhao@sjtu.edu.cn>
2023-04-23 16:11:31 +0800

    version2

--- dataprocess.py
+++ dataprocess.py
@@ -1,5 +1,13 @@
+import math
 import cv2 as cv
 import numpy as np
+
+def makeBOX(p1, p2, p3, p4):
+    return np.array([np.array(p1),
+                     np.array(p2),
+                     np.array(p3),
+                     np.array(p4),
+                     ])
 
 def find_plate(imageRGB) -> np.array :
     imageHSV = cv.cvtColor(imageRGB, cv.COLOR_BGR2HSV)
@@ -37,22 +45,38 @@ def find_plate(imageRGB) -> np.array :
     for candidate in contourCandidates:
         rect = cv.minAreaRect(candidate)
         width, height = rect[1]
+        theta = rect[2]
         if width < height:
             width, height = height, width
+            
         if width / height < 2.5 or width / height > 5.0:
             continue
         # print(width/height)
         box = cv.boxPoints(rect)
+        
+        ######## left or right ########
+        if theta > 45:
+            dy = height / math.sin(math.radians(theta))
+        else :
+            dy = height / math.cos(math.radians(theta))
+        x2, y2 = box[1]
+        x4, y4 = box[3]
+        x1 = x4
+        y1 = y4 - dy
+        x3 = x2
+        y3 = y2 + dy
+        box = makeBOX([x1, y1], [x2, y2], [x3, y3], [x4, y4])
+        # print(box)
+        
         boxes.append(np.intp(box))
     
     # print(len(boxes))
-    # print(boxes)
+    # print(boxes[0])
     imageMask = cv.drawContours(imageRGB, boxes, -1, (0, 0, 255), 5)
     cv.imwrite("test5.png", imageMask)
 
     
-
-
+    
 
 if __name__ == '__main__':
     image_path = "./images/difficult/3-2.jpg"
